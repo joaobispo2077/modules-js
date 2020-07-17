478ca2b9cb8e3d18a03d6783698937ac429461f0
João Bispo <vitor.vi10@hotmail.com>
2020-07-17 14:43:40 -0300

    Compose Functions

--- JS-Functional-Paradigm/public/app/notas/service.js
+++ JS-Functional-Paradigm/public/app/notas/service.js
@@ -1,18 +1,29 @@
 import { handleStatus } from '../utils/promise-helpers.js';
+import { partialize, pipe } from '../utils/operators.js'
 
 const API = 'http://localhost:3000/notas'
 
-const sumItems = code => notas => notas
-    .$flatMap(nota => nota.itens)
-    .filter(item => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
+
+const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+
 
 export const notasService = {
     listAll() {
-        return fetch(API).then(handleStatus);
+        return fetch(API)
+            .then(handleStatus)
+            .catch(err => {
+                console.log(err);
+                return Promise.reject('Não foi possível obter as notas fiscais')
+            })
     },
 
     sumItems(code) {
-        return this.listAll().then(sumItems(code))
+        const filterItems = partialize(filterItemsByCode, code);
+        const sumItems = pipe(getItemsFromNotas, filterItems, sumItemsValue);
+        return this.listAll()
+            .then(sumItems);
     }
 }
