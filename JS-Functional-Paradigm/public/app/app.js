7aa957519dc46219ab08a0f8a557b5604f624317
João Bispo <vitor.vi10@hotmail.com>
2021-02-22 12:19:31 -0300

    refact: RESTART Functional Programing with JS

--- JS-Functional-Paradigm/public/app/app.js
+++ JS-Functional-Paradigm/public/app/app.js
@@ -1,36 +1,3 @@
-import { log } from './utils/promise-helpers.js'
-import { notasService as service } from './notas/service.js'
-
-import './utils/array-helpers.js'
-
-
-
-
-
 document
-    .querySelector('#myButton')
-    .onclick = () =>
-    service
-    .sumItems('2143')
-    .then(log)
-    .catch(console.log)
-
-
-// [
-//     [{ codigo: '123' }, { valor: 100 }]
-// ]
-
-// [{ codigo: '123' }, { valor: 100 }]
-
-// [
-//     [
-//         { codigo: '123' },
-//         { valor: 100 }
-//     ]
-// ]
-
-
-// [
-//     { codigo: '123' },
-//     { valor: 100 }
-// ]
+  .querySelector('#myButton')
+  .catch(console.log)
--- JS-Functional-Paradigm/public/app/utils/promise-helpers.js
+++
@@ -1,6 +0,0 @@
-export const handleStatus = res => res.ok ? res.json() : Promise.reject(res.statusText)
-
-export const log = param => {
-    console.log(param);
-    return param;
-}
